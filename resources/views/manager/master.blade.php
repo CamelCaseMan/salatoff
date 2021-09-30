b1884951ab2cbab3ddb4b9e8aec6394e6ffd3a47
work3212 <48941393+work3212@users.noreply.github.com>
2021-09-30 12:00:33 +0300

    Панель менеджера

---
+++ resources/views/manager/master.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <!-- Обязательные метатеги -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Salatoff.ru</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('theme')}}/css/beauty.css"/>
+</head>
+<body>
+@include('front.menu')
+<div class="container">
+    <div id="page" style="margin-top: 50px;">
+        @yield('content')
+    </div>
+</div>
+
+<!-- Вариант 1: Bootstrap в связке с Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
