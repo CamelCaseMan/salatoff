faeba0a92707d0f22f41e9ea4b5fcd0bdb8adbfa
CamelCase <admin@vetrov.site>
2021-11-10 16:36:15 +0300

    Added steps functional.

--- resources/views/front/basket/registration.blade.php
+++ resources/views/front/basket/registration.blade.php
@@ -22,17 +22,17 @@
 		<div class="order__body step-tabs">
 
 			<div class="order__body-header">
-				<div class="-num">Шаг 1 из 5</div>
+				<div class="-num step-tabs-num"></div>
 				<div class="-line">
-					<div class="-passed" style="width: 20%;"></div>
+					<div class="-passed step-tabs-line"></div>
 				</div>
 			</div>
 
-			<form class="order__body-content">
+			<form action="/basket" class="order__body-content">
 
 				<div class="step-tabs-wrapper mb-40">
 
-					<div class="step-tabs-tab order__tab">
+					<div class="step-tabs-tab order__tab" id="form-tab-01">
 
 						<div class="order__body-title mb-40">
 							<div class="-title">Ваши данные</div>
@@ -45,7 +45,7 @@
 										<label class="-prefix" for="tab-001-name">Ваше имя <span class="-star">*</span></label>
 										<div class="valinput">
 											<!-- * Если успешно, то "--success", нет — "--error" -->
-											<input class="input-style --required" name="name" id="tab-001-name" type="text" placeholder="Ваше имя">
+											<input class="input-style --required" required name="name" id="tab-001-name" type="text" placeholder="Ваше имя">
 											<!-- * Это сообщение об ошибке -->
 											<div class="-error-message">Обязательное поле</div>
 											<!-- * Если нужно доп. сообщение под инпутом (размещать под -error-message): -->
@@ -70,7 +70,7 @@
 										<label class="-prefix" for="tab-001-phone">Контактный номер <span class="-star">*</span></label>
 										<div class="valinput">
 											<!-- * Если успешно, то "--success", нет — "--error" -->
-											<input class="input-style --required input-phone" name="phone" id="tab-001-phone" placeholder="+7 (___) ___ ____" type="tel">
+											<input class="input-style --required input-phone" required name="phone" id="tab-001-phone" placeholder="+7 (___) ___ ____" type="tel">
 											<!-- * Это сообщение об ошибке -->
 											<div class="-error-message">Обязательное поле</div>
 											<!-- * Если нужно доп. сообщение под инпутом (размещать под -error-message): -->
@@ -94,10 +94,10 @@
 
 					</div>
 					
-					<div class="step-tabs-tab order__tab">
+					<div class="step-tabs-tab order__tab" id="form-tab-02">
 
 						<div class="order__body-title mb-30">
-							<div class="-back">← Назад</div>
+							<div class="-back step-tabs-back">← Назад</div>
 							<div class="-title">Дата доставки</div>
 						</div>
 
@@ -116,7 +116,7 @@
 												<path fill-rule="evenodd" clip-rule="evenodd" d="M5 2H13V0H15V2H16C16.5304 2 17.0391 2.21071 17.4142 2.58579C17.7893 2.96086 18 3.46957 18 4V18C18 18.5304 17.7893 19.0391 17.4142 19.4142C17.0391 19.7893 16.5304 20 16 20H2C1.46957 20 0.960859 19.7893 0.585786 19.4142C0.210714 19.0391 0 18.5304 0 18V4C0 3.46957 0.210714 2.96086 0.585786 2.58579C0.960859 2.21071 1.46957 2 2 2H3V0H5V2ZM2 6V18H16V6H2ZM4 9H6V11H4V9ZM8 9H10V11H8V9ZM12 9H14V11H12V9ZM12 13H14V15H12V13ZM8 13H10V15H8V13ZM4 13H6V15H4V13Z" fill="#C4C4C4"/>
 											</svg>
 										</label>
-										<input readonly type="text" name="date" class="input-style" id="datepicker" placeholder="Выберите дату">
+										<input readonly type="text" required name="date" class="input-style" id="datepicker" placeholder="Выберите дату">
 										<!-- * Это сообщение об ошибке -->
 										<div class="-error-message">Обязательное поле</div>
 										<!-- * Если нужно доп. сообщение под инпутом (размещать под -error-message): -->
@@ -128,10 +128,10 @@
 
 					</div>
 
-					<div class="step-tabs-tab order__tab">
+					<div class="step-tabs-tab order__tab" id="form-tab-03">
 
 						<div class="order__body-title mb-30">
-							<div class="-back">← Назад</div>
+							<div class="-back step-tabs-back">← Назад</div>
 							<div class="-title">Адрес доставки</div>
 						</div>
 						
@@ -142,7 +142,7 @@
 								<section class="-form-section">
 									<label class="-prefix" for="tab-003-city">Город</label>
 									<div class="input-droplist">
-										<input id="tab-003-city" readonly value="Москва" class="input-style -input" tabindex="-1">
+										<input id="tab-003-city" name="city" required readonly value="Москва" class="input-style -input" tabindex="-1">
 										<div class="-focus-elements">
 											<svg class="-arrow" width="14" height="9" viewBox="0 0 14 9" fill="none">
 												<path d="M7 9L13.9282 0H0.0717969L7 9Z" fill="#C4C4C4"/>
@@ -161,7 +161,7 @@
 										<label class="-prefix" for="tab-003-street">Улица</label>
 										<div class="valinput">
 											<!-- * Если успешно, то "--success", нет — "--error" -->
-											<input class="input-style --required" name="street" id="tab-003-street" type="text">
+											<input class="input-style --required" required name="street" id="tab-003-street" type="text">
 											<!-- * Это сообщение об ошибке -->
 											<div class="-error-message">Обязательное поле</div>
 											<!-- * Если нужно доп. сообщение под инпутом (размещать под -error-message): -->
@@ -178,7 +178,7 @@
 										<label class="-prefix" for="tab-003-home">Дом</label>
 										<div class="valinput">
 												<!-- * Если успешно, то "--success", нет — "--error" -->
-												<input class="input-style --required" name="home" id="tab-003-home" type="text">
+												<input class="input-style --required" required name="home" id="tab-003-home" type="text">
 												<!-- * Это сообщение об ошибке -->
 												<div class="-error-message">Обязательное поле</div>
 												<!-- * Если нужно доп. сообщение под инпутом (размещать под -error-message): -->
@@ -189,7 +189,7 @@
 										<label class="-prefix" for="tab-003-prehome">Подъезд</label>
 										<div class="valinput">
 												<!-- * Если успешно, то "--success", нет — "--error" -->
-												<input class="input-style --required" name="prehome" id="tab-003-prehome" type="text">
+												<input class="input-style --required" required name="entrance" id="tab-003-prehome" type="text">
 												<!-- * Это сообщение об ошибке -->
 												<div class="-error-message">Обязательное поле</div>
 												<!-- * Если нужно доп. сообщение под инпутом (размещать под -error-message): -->
@@ -202,7 +202,7 @@
 										<label class="-prefix" for="tab-003-floor">Этаж</label>
 										<div class="valinput">
 												<!-- * Если успешно, то "--success", нет — "--error" -->
-												<input class="input-style --required" name="floor" id="tab-003-floor" type="text">
+												<input class="input-style --required" required name="floor" id="tab-003-floor" type="text">
 												<!-- * Это сообщение об ошибке -->
 												<div class="-error-message">Обязательное поле</div>
 												<!-- * Если нужно доп. сообщение под инпутом (размещать под -error-message): -->
@@ -213,7 +213,7 @@
 										<label class="-prefix" for="tab-003-office">Квартира/офис</label>
 										<div class="valinput">
 												<!-- * Если успешно, то "--success", нет — "--error" -->
-												<input class="input-style --required" name="office" id="tab-003-office" type="text">
+												<input class="input-style --required" required name="office" id="tab-003-office" type="text">
 												<!-- * Это сообщение об ошибке -->
 												<div class="-error-message">Обязательное поле</div>
 												<!-- * Если нужно доп. сообщение под инпутом (размещать под -error-message): -->
@@ -226,10 +226,10 @@
 
 					</div>
 
-					<div class="step-tabs-tab order__tab active">
+					<div class="step-tabs-tab order__tab" id="form-tab-04">
 
 						<div class="order__body-title mb-30">
-							<div class="-back">← Назад</div>
+							<div class="-back step-tabs-back">← Назад</div>
 							<div class="-title">Способ оплаты</div>
 						</div>
 						
@@ -262,7 +262,7 @@
 							<section class="-form-section">
 								<div class="valinput">
 									<!-- * Если успешно, то "--success", нет — "--error" -->
-									<textarea class="input-style --required" name="home" rows="6" placeholder="Если мы должны что-то знать дополнительно, напишите об этом здесь "></textarea>
+									<textarea class="input-style --required" name="comment" rows="6" placeholder="Если мы должны что-то знать дополнительно, напишите об этом здесь "></textarea>
 									<!-- * Это сообщение об ошибке -->
 									<div class="-error-message">Обязательное поле</div>
 									<!-- * Если нужно доп. сообщение под инпутом (размещать под -error-message): -->
@@ -273,12 +273,115 @@
 
 					</div>
 
+					<div class="step-tabs-tab step-tabs-checkout-tab order__tab">
+
+						<div class="order__body-title mb-60">
+							<div class="-back step-tabs-back">← Назад</div>
+							<div class="-title">Проверим, всё ли верно</div>
+						</div>
+
+						<section class="-checkout-section">
+							<div class="-title">
+								<div class="-text">Ваши данные</div>
+								<svg class="-icon step-tabs-trigger" data-num="0" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M11 4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V20C2 20.5304 2.21071 21.0391 2.58579 21.4142C2.96086 21.7893 3.46957 22 4 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V13" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+									<path d="M18.5 2.50023C18.8978 2.1024 19.4374 1.87891 20 1.87891C20.5626 1.87891 21.1022 2.1024 21.5 2.50023C21.8978 2.89805 22.1213 3.43762 22.1213 4.00023C22.1213 4.56284 21.8978 5.1024 21.5 5.50023L12 15.0002L8 16.0002L9 12.0002L18.5 2.50023Z" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+								</svg>
+							</div>
+							<p>
+								Ваше имя: Валентин Константинович <br>
+								Организация: ОАО «Все свои» <br>
+								Контактный номер: +7 (916) 000 15 10
+							</p>
+						</section>
+
+						<section class="-checkout-section">
+							<div class="-title">
+								<div class="-text">Дата доставки</div>
+								<svg class="-icon step-tabs-trigger" data-num="1" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M11 4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V20C2 20.5304 2.21071 21.0391 2.58579 21.4142C2.96086 21.7893 3.46957 22 4 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V13" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+									<path d="M18.5 2.50023C18.8978 2.1024 19.4374 1.87891 20 1.87891C20.5626 1.87891 21.1022 2.1024 21.5 2.50023C21.8978 2.89805 22.1213 3.43762 22.1213 4.00023C22.1213 4.56284 21.8978 5.1024 21.5 5.50023L12 15.0002L8 16.0002L9 12.0002L18.5 2.50023Z" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+								</svg>
+							</div>
+							<p>
+								16.12.2021
+							</p>
+						</section>
+
+						<section class="-checkout-section">
+							<div class="-title">
+								<div class="-text">Адрес доставки</div>
+								<svg class="-icon step-tabs-trigger" data-num="2" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M11 4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V20C2 20.5304 2.21071 21.0391 2.58579 21.4142C2.96086 21.7893 3.46957 22 4 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V13" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+									<path d="M18.5 2.50023C18.8978 2.1024 19.4374 1.87891 20 1.87891C20.5626 1.87891 21.1022 2.1024 21.5 2.50023C21.8978 2.89805 22.1213 3.43762 22.1213 4.00023C22.1213 4.56284 21.8978 5.1024 21.5 5.50023L12 15.0002L8 16.0002L9 12.0002L18.5 2.50023Z" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+								</svg>
+							</div>
+							<p>
+								Москва, ул. Маршала Катукова, д. 13 <br>
+								Подъезд 4, этаж 3
+							</p>
+						</section>
+
+						<section class="-checkout-section">
+							<div class="-title">
+								<div class="-text">Способ оплаты</div>
+								<svg class="-icon step-tabs-trigger" data-num="3" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M11 4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V20C2 20.5304 2.21071 21.0391 2.58579 21.4142C2.96086 21.7893 3.46957 22 4 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V13" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+									<path d="M18.5 2.50023C18.8978 2.1024 19.4374 1.87891 20 1.87891C20.5626 1.87891 21.1022 2.1024 21.5 2.50023C21.8978 2.89805 22.1213 3.43762 22.1213 4.00023C22.1213 4.56284 21.8978 5.1024 21.5 5.50023L12 15.0002L8 16.0002L9 12.0002L18.5 2.50023Z" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+								</svg>
+							</div>
+							<p>
+								Картой онлайн
+							</p>
+						</section>						
+
+						<section class="-checkout-section">
+							<div class="-title">
+								<div class="-text">Комментарий к заказу</div>
+								<svg class="-icon step-tabs-trigger" data-num="3" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M11 4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V20C2 20.5304 2.21071 21.0391 2.58579 21.4142C2.96086 21.7893 3.46957 22 4 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V13" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+									<path d="M18.5 2.50023C18.8978 2.1024 19.4374 1.87891 20 1.87891C20.5626 1.87891 21.1022 2.1024 21.5 2.50023C21.8978 2.89805 22.1213 3.43762 22.1213 4.00023C22.1213 4.56284 21.8978 5.1024 21.5 5.50023L12 15.0002L8 16.0002L9 12.0002L18.5 2.50023Z" stroke="#6E6E6E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+								</svg>
+							</div>
+							<p>
+								Пожалуйста, не кладите пластиковые приборы
+							</p>
+						</section>
+
+						<div class="order__tab-total">
+							<div class="-top">
+								<div class="-title">Итого</div>
+								<ul>
+									<li>
+										<div class="-item">1. Салат свекольный с майонезом</div>
+										<div class="-price">260 ₽</div>
+									</li>
+									<li>
+										<div class="-item">2. Крем-суп тыквенный</div>
+										<div class="-price">105 ₽</div>
+									</li>
+								</ul>
+							</div>
+							<ul class="-bottom">
+								<li>
+									<div class="-item">Скидка</div>
+									<div class="-price">10 ₽</div>
+								</li>
+								<li>
+									<div class="-item">К оплате</div>
+									<div class="-price">155 ₽</div>
+								</li>
+							</ul>
+						</div>
+
+					</div>
+
 				</div>
 
 				<div class="order__body-buttons">
 					<div class="-split-buttons">
-						<div class="-button typical-button d-block text-center --square --stroke --thin">Отмена</div>
-						<div class="-button typical-button d-block text-center --square">
+						<a href="/basket" class="-button typical-button d-block text-center --square --stroke --thin">Отмена</a>
+						<div class="-button typical-button d-block text-center --square step-tabs-next">
 							Далее
 							<svg width="12" height="11" viewBox="0 0 12 11" fill="none">
 								<path d="M9.26403 6.05598H0.400024V4.74398H9.26403L5.12002 0.599976H6.96002L11.76 5.39998L6.96002 10.2H5.12002L9.26403 6.05598Z" fill="#1A1A1A"/>
@@ -286,8 +389,8 @@
 						</div>
 					</div>
 					<div class="-finish-buttons">
-						<div class="-button typical-button d-block text-center --square">Оформить заказ</div>
-						<div class="-button typical-button d-block text-center --square --stroke --thin">Отменить ввод данных</div>
+						<button type="submit" class="-button typical-button d-block text-center --square">Оформить заказ</button>
+						<a href="/basket" class="-button typical-button d-block text-center --square --stroke --thin">Отменить ввод данных</a>
 					</div>
 				</div>
 				
