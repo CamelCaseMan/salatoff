90a9ca19458f5612957d816425e9eb45390352f3
CamelCase <admin@vetrov.site>
2021-11-29 01:54:02 +0300

    Contacts page

--- resources/views/front/page/contacts.blade.php
+++ resources/views/front/page/contacts.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="our-production-page container mb-160 l-mb-120">
+<div class="contacts-page container mb-160 l-mb-120">
 
 	<div class="product-page__back mb-30 xs-mb-20">
 			<a href="{{ URL::previous() }}">
@@ -16,6 +16,50 @@
 
 	<h2 class="second-title">Контактная информация</h2>
 
-	
+	<div class="contacts__split">
+		<div class="contacts__info">
+			<div class="contacts__info-block">
+				<div class="-title">ООО «Кухня №1»</div>
+				<p class="-text">
+					ИНН/КПП 7726752821/772601001  <br>
+					Юридический адрес: Россия, г Москва, ул. Кировоградская 22 Г. <br>
+					Фактический адрес: Россия, г Москва, ул. Кировоградская 22 Г. <br>
+					Тел./Факс: +7 (495) 797 64 57
+				</p>
+			</div>
+			<div class="contacts__info-block --important">
+				<div class="-title">Руководитель отдела продаж</div>
+				<p class="-text">
+					Позднякова Наиля: +7 (964) 504-48-70 <br>
+					Nailya@salatoff.ru 
+				</p>
+			</div>
+			<div class="contacts__info-block">
+				<div class="-title">Предложения по поставкам сырья</div>
+				<p>
+					Менеджер отдела закупок Ирина Ким: +7 (921) 992 70 87<br>
+					0707816@mail.ru
+				</p>
+			</div>
+			<div class="contacts__info-block">
+				<div class="-title">Мы можем организовать дегустацию на вашем предприятии или в сети наших кулинарий-кондитерских "Беззабот" по адресам</div>
+				<p class="-text">
+					МО, г. Одинцово, ул. Молодежная д. 46 <br>	
+					МО, г .Одинцово, улица Говорова д. 24А.
+				</p>
+			</div>
+		</div>
+
+		<div class="contacts__images">
+			<div class="-img" style="background-image: url({{asset('theme')}}/img/contacts/1.jpeg);"></div>
+			<div class="-img" style="background-image: url({{asset('theme')}}/img/contacts/2.jpg);"></div>
+			<div class="-img" style="background-image: url({{asset('theme')}}/img/contacts/3.jpeg);"></div>
+			<div class="-img" style="background-image: url({{asset('theme')}}/img/contacts/4.jpeg);"></div>
+			<div class="-img" style="background-image: url({{asset('theme')}}/img/contacts/5.jpeg);"></div>
+			<div class="-img" style="background-image: url({{asset('theme')}}/img/contacts/6.jpeg);"></div>
+		</div>
+	</div>
+
+</div>
 
 @endsection
