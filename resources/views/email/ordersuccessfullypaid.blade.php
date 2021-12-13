ff749c392615eddf62f5c8b03b66e3545f4911d2
work3212 <48941393+work3212@users.noreply.github.com>
2021-12-13 11:31:48 +0300

    Правка письма юкассы

--- resources/views/email/ordersuccessfullypaid.blade.php
+++ resources/views/email/ordersuccessfullypaid.blade.php
@@ -1,10 +1,8 @@
 @component('mail::message')
     # Клиент оплатил заказ онлайн
-
-    @component('mail::panel')
-        <p>Номер заказа: {{$order_id}}</p>
-    @endcomponent
+    <h3>Номер заказа {{$order_id}}</h3>
 
     Поддержка сайта <br>
     <a href="mailto:work3212@mail.ru">salatoff.ru</a>
 @endcomponent
+
